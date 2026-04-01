04e73c9406f0c5ba7aa7839ed3cb27d2b260788c
Ambady Dileep <ambadydileep.pro@gmail.com>
2026-04-01 21:17:59 +0530

    Backend issue resolved

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),  # token validity
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
